9995153c3eb42070b6c39f021e20acd156e3e927
Crystal <magicrystalorginal@gmail.com>
2024-05-19 17:00:17 +0200

    jsp

--- src/main/java/net/fullcrystal/tutorialmod/TutorialMod.java
+++ src/main/java/net/fullcrystal/tutorialmod/TutorialMod.java
@@ -24,7 +24,7 @@ public class TutorialMod
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-
+//test
     public TutorialMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
